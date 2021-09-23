82f360b9017e8c3db5c4eff1e926e3e62ad1e3c5
Zixin Zhang <zixinzha@seas.upenn.edu>
2021-09-22 20:14:53 -0400

    Level 1

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -6,7 +6,9 @@ public class Oscillator : MonoBehaviour
 {
     Vector3 startingPos;
     [SerializeField] Vector3 movementVector;
-    [SerializeField] [Range(0,1)] float movementFactor;
+    float movementFactor;
+    [SerializeField] float period = 2f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +18,19 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // continually growing over time
+        // note here we don't do == 0 because of precision issue
+        float cycles = period <= Mathf.Epsilon ? 0.0f : Time.time / period; 
+
+        // tau is a constant value of 6.283
+        const float tau = Mathf.PI * 2; 
+
+        // going from -1 to 1
+        float rawSinWave = Mathf.Sin(cycles * tau);
+
+        // going from 0 to 1
+        movementFactor = (rawSinWave + 1.0f) / 2.0f;
+
         Vector3 offset = movementVector * movementFactor;
         transform.position = startingPos + offset;
     }
